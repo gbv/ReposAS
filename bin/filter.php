5684a3cd05e2865dbac136ac75d60e026efb6928
Maximilian Salomon <salomon@zib.de>
2019-10-21 18:22:34 +0200

    Kleine Namensanpassung

--- bin/filter.php
+++ bin/filter.php
@@ -5,14 +5,14 @@ require_once __DIR__.'/../config/config.php';
 require_once __DIR__.'/../vendor/autoload.php';
 
 $convertedLoglineParser= new ReposAS\ConvertedLoglineParser();
-$convertedFilterRobots= new ReposAS\FilterRobots ();
+$filterRobots= new ReposAS\FilterRobots ();
 $counter3Filter30sek = new ReposAS\Counter3Filter30sek ();
 
 while (! feof(STDIN)) {
     if ($line = trim(fgets(STDIN))) {
         $logline=new ReposAS\ConvertedLogline();
         if ( $convertedLoglineParser->parse($line, $logline)) {
-            $convertedFilterRobots->edit($logline);
+            $filterRobots->edit($logline);
             $counter3Filter30sek->edit($logline);
             echo ($logline."\n");
         } else {
