4a088bbde5386d305875281ecab6f7de7a2dc978
Paul Borchert <Paul.Borchert@gbv.de>
2019-12-13 18:38:27 +0100

    add compare to identifer for 30 sek filter

--- logfileparser/bin/lib/counter3-filter-30sek.php
+++ logfileparser/bin/lib/counter3-filter-30sek.php
@@ -11,19 +11,31 @@ class Counter3Filter30sek {
         $ip=$reposasLogline->IP;
         $path=$reposasLogline->URL;
         $time=$reposasLogline->Time;
+        $identifier=$reposasLogline->Identifier;
         $unixtime=strtotime($time);
         // delete old entrys
         while (count($this->lastHits) > 0 && $unixtime - key($this->lastHits) > 30) {
             array_shift($this->lastHits);
         }
         // Find duplicate entry
-        foreach ($this->lastHits as $lastHit) {
-            if ($lastHit['ip'] == $ip && $lastHit['path']==$path) {
-                $reposasLogline->Subjects[]="filter:30sek:counter3";
+        foreach ($this->lastHits as $lastHitsForSec) {
+            foreach ($lastHitsForSec as $lastHit) {
+               if ($lastHit['ip'] == $ip && 
+                        ($lastHit['path']==$path || 
+                            ( count($identifier) > 0 && count(array_diff($identifier,$lastHit['identifier'])) == 0)
+                        )
+                  ) {
+                    $reposasLogline->Subjects[]="filter:30sek:counter3";
+               }
             }
         }
-        $this->lastHits[$unixtime]['ip']=$ip;
-        $this->lastHits[$unixtime]['path']=$path;
+        if (! isset($this->lastHits[$unixtime])) $this->lastHits[$unixtime]=array();
+        $thisHit=array();
+        $thisHit['ip']=$ip;
+        $thisHit['path']=$path;
+        $thisHit['identifier']=$identifier;
+        $this->lastHits[$unixtime][]=$thisHit;
+        
     }
 }
 ?>
